97c68b58df23c93a4f430ba7057d0d0a4b335abb
CousinBenjy <benjamin.anscombe@hotmail.com>
2019-10-20 14:26:08 +0100

    Created the VS project and created a basic window form design

---
+++ CHIP-8 Interpreter/Window.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CHIP_8_Interpreter
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InterpreterOutput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.InterpreterOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InterpreterOutput
+            // 
+            this.InterpreterOutput.Location = new System.Drawing.Point(142, 141);
+            this.InterpreterOutput.Name = "InterpreterOutput";
+            this.InterpreterOutput.Size = new System.Drawing.Size(320, 160);
+            this.InterpreterOutput.TabIndex = 1;
+            this.InterpreterOutput.TabStop = false;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.InterpreterOutput);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Window";
+            this.Text = "CHIP-8 Interpreter";
+            ((System.ComponentModel.ISupportInitialize)(this.InterpreterOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox InterpreterOutput;
+    }
+}
+
---
+++ CHIP-8 Interpreter/Window.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CHIP_8_Interpreter
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectROMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
